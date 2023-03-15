6b145cd4c208a9d0ba4234e0608b24496f38bc28
Pau Quintas <pau.quintas@e-campus.uab.cat>
2023-03-15 02:27:11 +0100

    downloadimage on front

--- app-TFG/src/app/image-loader/image-loader.component.ts
+++ app-TFG/src/app/image-loader/image-loader.component.ts
@@ -11,21 +11,34 @@ export class ImageLoaderComponent {
   selectedImage: File = {} as File;
   isUploaded: boolean = false;
   public imageSrc: string = "";
+  nameImage: string = "";
+  isSent: boolean = false;
 
   constructor(private http: HttpClient) { }
 
   sendImage(event: any) {
     this.selectedImage = <File>event.target.files[0];
     this.isUploaded = true;
+    this.nameImage = this.selectedImage.name;
   }
 
   sendToBack() {
-    console.log(this.selectedImage)
     const formData = new FormData();
     formData.append('image', this.selectedImage, this.selectedImage.name);
-    console.log(formData);
     this.http.post('/api/uploadImage', formData).subscribe((data: any) => {
       this.imageSrc = 'data:image/jpg;base64,' + data.image;
+    })
+    this.isSent = true;
+  }
+
+  downloadImage() {
+    this.http.get('/api/downloadImageURL',{ responseType: 'blob'})
+    .subscribe((response)=>{
+      const blobUrl = URL.createObjectURL(response);
+      const downloadLink = document.createElement('a');
+      downloadLink.href = blobUrl;
+      downloadLink.download = this.nameImage; // Change the filename and extension as needed
+      downloadLink.click();
     })
   }
 }
