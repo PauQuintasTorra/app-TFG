f9bc37798a49dc49bf1f0e688aa0236a731c7e20
Pau Quintas <pau.quintas@e-campus.uab.cat>
2023-03-19 23:30:11 +0100

    clean front and not working back download

--- app-TFG/src/app/image-loader/image-loader.component.ts
+++ app-TFG/src/app/image-loader/image-loader.component.ts
@@ -8,18 +8,24 @@ import { HttpClient, HttpRequest } from '@angular/common/http';
 })
 export class ImageLoaderComponent {
 
-  selectedImage: File = {} as File;
-  isUploaded: boolean = false;
-  public imageSrc: string = "";
-  nameImage: string = "";
-  isSent: boolean = false;
-  selectedFormat: string = 'jpg';
   constructor(private http: HttpClient) { }
 
-  imprimir(){
-    console.log(this.selectedFormat);
-  }
-  sendImage(event: any) {
+  public selectedImage: File = {} as File;
+
+  public imageSrc: string = "";
+  public nameImage: string = "";
+  public selectedFormat: string = 'jpg';
+  public nameSettedImage: string = "";
+
+  public isSent: boolean = false;
+  public isFormatChange: boolean = false;
+  public isUploaded: boolean = false;
+
+  // imprimir(){
+  //   console.log(this.selectedFormat);
+  // }
+
+  selectImage(event: any) {
     this.selectedImage = <File>event.target.files[0];
     this.isUploaded = true;
     this.nameImage = this.selectedImage.name.split('.')[0];
@@ -34,15 +40,23 @@ export class ImageLoaderComponent {
       this.imageSrc = `data:image/${this.selectedFormat};base64,` + data.image;
     })
     this.isSent = true;
+    this.isFormatChange = true;
   }
 
   downloadImage() {
-    this.http.get('/api/downloadImageURL',{ responseType: 'blob'})
-    .subscribe((response)=>{
-      const blobUrl = URL.createObjectURL(response);
+    const format = this.selectedFormat;
+    this.http.post('/api/downloadImageURL', {format})
+    .subscribe((response: any)=>{
+      console.log(format);
+      const blob = new Blob([response], { type: `image/${format}` });
+      const blobUrl = URL.createObjectURL(blob);
       const downloadLink = document.createElement('a');
       downloadLink.href = blobUrl;
-      downloadLink.download = `${this.nameImage}.${this.selectedFormat}`; 
+      if (this.nameSettedImage === "") {
+        downloadLink.download = `${this.nameImage}.${this.selectedFormat}`;
+      } else {
+        downloadLink.download = `${this.nameSettedImage}.${this.selectedFormat}`;
+      }
       downloadLink.click();
     })
   }
