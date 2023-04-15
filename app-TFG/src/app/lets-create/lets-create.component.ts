ca29a043989624459be17a0fd582d21a6fbe9a4e
Pau Quintas <1564806@uab.cat>
2023-04-15 16:02:48 +0200

    changing plus/arrow and insert  arrow veure button

--- app-TFG/src/app/lets-create/lets-create.component.ts
+++ app-TFG/src/app/lets-create/lets-create.component.ts
@@ -10,38 +10,58 @@ import { BoxTypeDialogComponent } from '../box-type-dialog/box-type-dialog.compo
   styleUrls: ['./lets-create.component.css'],
 })
 export class LetsCreateComponent {
-
   public selectedImage: File = {} as File;
   public isUploaded: boolean = false;
   public isFormatChange: boolean = false;
   public isSent: boolean = false;
   public nameImage: string = '';
-  public imageSrc: string = "";
+  public imageSrc: string = '';
   public originalFormat: string = '';
+  public selectedOption = 'Wavelet';
+  public numBoxes = 0;
+  public boxes!: Boxes;
+  public deletedNum: number[] = [];
+  public isArrowDraw: boolean = false;
 
-  constructor(private elementRef: ElementRef, private http: HttpClient, private dialog: MatDialog) {
-    this.boxes = new Boxes;
+  constructor(
+    private elementRef: ElementRef,
+    private http: HttpClient,
+    private dialog: MatDialog
+  ) {
+    this.boxes = new Boxes();
   }
 
   @ViewChild('boxContainer') boxContainer!: ElementRef;
 
-  selectedOption = 'Wavelet';
-  numBoxes = 0;
-  boxes!: Boxes;
-  operatorBox: any;
-  deletedNum: number[] = [];
-
-
   selectImage(event: any) {
     this.selectedImage = <File>event.target.files[0];
     this.isUploaded = true;
-    let name = this.selectedImage.name.split('')
+    let name = this.selectedImage.name.split('');
     this.nameImage = name[0];
     this.originalFormat = name[name.length - 1];
   }
-  
+
   seePhoto() {
-    console.log(this.boxes)
+    if (!this.isArrowDraw) {
+      const lastBox = document.getElementById(
+        `${this.boxes.box[this.boxes.box.length - 1].numberBox}`
+      );
+      if (lastBox) {
+        const newIcon = document.createElement('i');
+        newIcon.classList.add(
+          'fa-solid',
+          'fa-arrow-right',
+          'fa-beat',
+          'fa-2xl'
+        );
+        newIcon.style.position = 'absolute';
+        newIcon.style.top = '50%';
+        newIcon.style.left = `${parseInt(lastBox.style.width) + 20}px`;
+        newIcon.style.transform = 'translateY(-50%)';
+        lastBox.appendChild(newIcon);
+      }
+    }
+    console.log(this.boxes);
     const formData = new FormData();
     formData.append('image', this.selectedImage, this.selectedImage.name);
     formData.append('originalFormat', this.originalFormat);
@@ -49,7 +69,7 @@ export class LetsCreateComponent {
 
     this.http.post('/api/seeImage', formData).subscribe((data: any) => {
       // this.imageSrc = `data:image/${this.originalFormat};base64,` + data.image;
-    })
+    });
     this.isSent = true;
     this.isFormatChange = true;
   }
@@ -59,21 +79,20 @@ export class LetsCreateComponent {
       width: '400px',
       height: '300px',
     });
-  
+
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         console.log(result);
         this.selectedOption = result.type;
-        
+
         this.executeSelectedOption(result);
       }
     });
   }
-  
+
   executeSelectedOption(resultClass: any) {
     switch (this.selectedOption) {
       case 'Wavelet':
-        this.operatorBox = new Wavelet();
         this.createBox('green', resultClass);
         break;
       case 'ArithmeticOperation':
@@ -91,7 +110,7 @@ export class LetsCreateComponent {
   // const elementToRemove = document.getElementById('my-element');
   // elementToRemove.remove();
 
-  createNormalBox(color: string, resultClass: any){
+  createNormalBox(color: string, resultClass: any) {
     var box = new Box();
     box.numberBox = this.numBoxes;
     box.class = resultClass;
@@ -108,21 +127,20 @@ export class LetsCreateComponent {
     newBox.style.alignItems = 'center';
     newBox.style.justifyContent = 'center';
     newBox.style.display = 'flex';
-    newBox.style.flexDirection ='column';
-    
+    newBox.style.flexDirection = 'column';
+
     this.elementRef.nativeElement.appendChild(newBox);
     box.dashedBox = newBox;
     this.boxes?.box.push(box);
 
     const newDelete = document.createElement('button');
-    // establece las propiedades del icono
     newDelete.textContent = 'DELETE';
     newDelete.id = `delete_${this.numBoxes}`;
-    newDelete.addEventListener('click', () =>{
+    newDelete.addEventListener('click', () => {
       const parent = newDelete.parentElement;
       parent?.remove();
       this.deleteBox(parent?.id);
-    })
+    });
 
     newDelete.style.position = 'absolute';
     newDelete.style.top = `${parseInt(newBox.style.height) + 20}px`;
@@ -136,7 +154,7 @@ export class LetsCreateComponent {
     span.style.fontWeight = 'bold';
     span.style.color = color;
     newBox.appendChild(span);
-    
+
     switch (resultClass.type) {
       case 'Wavelet':
         const span1w = document.createElement('span');
@@ -163,63 +181,72 @@ export class LetsCreateComponent {
         span2a.style.fontWeight = 'bold';
         newBox.appendChild(span2a);
         break;
-      case 'Quantizer': 
+      case 'Quantizer':
         const span1q = document.createElement('span');
         span1q.classList.add('text');
         span1q.textContent = `Pas de Quantització: ${resultClass.q_step}`;
         span1q.style.fontWeight = 'bold';
         newBox.appendChild(span1q);
         break;
-    
+
       default:
         break;
     }
 
-    if (this.numBoxes < 3) {
+    if (this.numBoxes < 3 && this.numBoxes != 0) {
       const newIcon = document.createElement('i');
       newIcon.classList.add('fas', 'fa-plus');
-      // establece las propiedades del icono
       newIcon.id = `icon_${this.numBoxes}`;
       newIcon.style.position = 'absolute';
       newIcon.style.top = '50%';
-      newIcon.style.left = `${parseInt(newBox.style.width) + 20}px`;
+      newIcon.style.right = `${parseInt(newBox.style.width) + 20}px`;
       newIcon.style.transform = 'translateY(-50%)';
-
       newBox.appendChild(newIcon);
     } else {
-      const newIcon = document.createElement('i');
-      newIcon.classList.add(
-        'fa-solid',
-        'fa-arrow-right',
-        'fa-beat',
-        'fa-2xl'
-      );
-      // establece las propiedades del icono
-      newIcon.style.position = 'absolute';
-      newIcon.style.top = '50%';
-      newIcon.style.left = `${parseInt(newBox.style.width) + 20}px`;
-      newIcon.style.transform = 'translateY(-50%)';
-      newBox.appendChild(newIcon);
+      if (this.numBoxes == 3) {
+        this.isArrowDraw = true;
+        const newIconI = document.createElement('i');
+        newIconI.classList.add('fas', 'fa-plus');
+        newIconI.id = `icon_${this.numBoxes}`;
+        newIconI.style.position = 'absolute';
+        newIconI.style.top = '50%';
+        newIconI.style.right = `${parseInt(newBox.style.width) + 20}px`;
+        newIconI.style.transform = 'translateY(-50%)';
+        newBox.appendChild(newIconI);
+        const newIcon = document.createElement('i');
+        newIcon.classList.add(
+          'fa-solid',
+          'fa-arrow-right',
+          'fa-beat',
+          'fa-2xl'
+        );
+        newIcon.style.position = 'absolute';
+        newIcon.style.top = '50%';
+        newIcon.style.left = `${parseInt(newBox.style.width) + 20}px`;
+        newIcon.style.transform = 'translateY(-50%)';
+        newBox.appendChild(newIcon);
+      }
     }
   }
 
   createBox(color: string, resultClass: any) {
     if (this.numBoxes < 4) {
-      if(this.deletedNum.length == 0){
+      if (this.deletedNum.length == 0) {
         this.createNormalBox(color, resultClass);
       } else {
-        this.createSpaceBox(this.deletedNum.pop() as number, color, resultClass );
-      } 
+        this.createSpaceBox(
+          this.deletedNum.pop() as number,
+          color,
+          resultClass
+        );
+      }
       this.numBoxes++;
     } else {
       alert("You can't create more than 4 boxes");
     }
-
-
-    
   }
 
-  createSpaceBox(numBox: number, color: string, resultClass: any){
+  createSpaceBox(numBox: number, color: string, resultClass: any) {
     var box = new Box();
     box.numberBox = numBox;
     box.class = resultClass;
@@ -233,19 +260,23 @@ export class LetsCreateComponent {
     newBox.style.top = '35%';
     newBox.style.left = 200 + numBox * 250 + 'px';
     newBox.style.transform = 'translateX(-50%)';
+    newBox.style.alignItems = 'center';
+    newBox.style.justifyContent = 'center';
+    newBox.style.display = 'flex';
+    newBox.style.flexDirection = 'column';
+
     this.elementRef.nativeElement.appendChild(newBox);
     box.dashedBox = newBox;
     this.boxes?.box.splice(numBox, 0, box);
 
     const newDelete = document.createElement('button');
-    // establece las propiedades del icono
     newDelete.textContent = 'DELETE';
     newDelete.id = `delete_${numBox}`;
-    newDelete.addEventListener('click', () =>{
+    newDelete.addEventListener('click', () => {
       const parent = newDelete.parentElement;
       parent?.remove();
       this.deleteBox(parent?.id);
-    })
+    });
 
     newDelete.style.position = 'absolute';
     newDelete.style.top = `${parseInt(newBox.style.height) + 20}px`;
@@ -253,50 +284,106 @@ export class LetsCreateComponent {
 
     newBox.appendChild(newDelete);
 
-    if (numBox != 3) {
+    const span = document.createElement('span');
+    span.classList.add('text');
+    span.textContent = `${resultClass.type} `;
+    span.style.fontWeight = 'bold';
+    span.style.color = color;
+    newBox.appendChild(span);
+
+    switch (resultClass.type) {
+      case 'Wavelet':
+        const span1w = document.createElement('span');
+        span1w.classList.add('text');
+        span1w.textContent = `Nivells: ${resultClass.waveletLevel}`;
+        span1w.style.fontWeight = 'bold';
+        newBox.appendChild(span1w);
+        const span2w = document.createElement('span');
+        span2w.classList.add('text');
+        span2w.textContent = `Tipus: ${resultClass.waveletType}`;
+        span2w.style.fontWeight = 'bold';
+        newBox.appendChild(span2w);
+        break;
+
+      case 'ArithmeticOperation':
+        const span1a = document.createElement('span');
+        span1a.classList.add('text');
+        span1a.textContent = `Valor: ${resultClass.operationNumber}`;
+        span1a.style.fontWeight = 'bold';
+        newBox.appendChild(span1a);
+        const span2a = document.createElement('span');
+        span2a.classList.add('text');
+        span2a.textContent = `Tipus: ${resultClass.operationType}`;
+        span2a.style.fontWeight = 'bold';
+        newBox.appendChild(span2a);
+        break;
+      case 'Quantizer':
+        const span1q = document.createElement('span');
+        span1q.classList.add('text');
+        span1q.textContent = `Pas de Quantització: ${resultClass.q_step}`;
+        span1q.style.fontWeight = 'bold';
+        newBox.appendChild(span1q);
+        break;
+
+      default:
+        break;
+    }
+
+    if (numBox < 3 && numBox != 0) {
       const newIcon = document.createElement('i');
       newIcon.classList.add('fas', 'fa-plus');
-      // establece las propiedades del icono
       newIcon.id = `icon_${numBox}`;
       newIcon.style.position = 'absolute';
       newIcon.style.top = '50%';
-      newIcon.style.left = `${parseInt(newBox.style.width) + 20}px`;
+      newIcon.style.right = `${parseInt(newBox.style.width) + 20}px`;
       newIcon.style.transform = 'translateY(-50%)';
 
       newBox.appendChild(newIcon);
     } else {
-      const newIcon = document.createElement('i');
-      newIcon.classList.add(
-        'fa-solid',
-        'fa-arrow-right',
-        'fa-beat',
-        'fa-2xl'
-      );
-
-      newIcon.style.position = 'absolute';
-      newIcon.style.top = '50%';
-      newIcon.style.left = `${parseInt(newBox.style.width) + 20}px`;
-      newIcon.style.transform = 'translateY(-50%)';
-      newBox.appendChild(newIcon);
+      if (this.numBoxes == 3) {
+        this.isArrowDraw = true;
+        const newIconI = document.createElement('i');
+        newIconI.classList.add('fas', 'fa-plus');
+        newIconI.id = `icon_${this.numBoxes}`;
+        newIconI.style.position = 'absolute';
+        newIconI.style.top = '50%';
+        newIconI.style.right = `${parseInt(newBox.style.width) + 20}px`;
+        newIconI.style.transform = 'translateY(-50%)';
+        newBox.appendChild(newIconI);
+        const newIcon = document.createElement('i');
+        newIcon.classList.add(
+          'fa-solid',
+          'fa-arrow-right',
+          'fa-beat',
+          'fa-2xl'
+        );
+        newIcon.style.position = 'absolute';
+        newIcon.style.top = '50%';
+        newIcon.style.left = `${parseInt(newBox.style.width) + 20}px`;
+        newIcon.style.transform = 'translateY(-50%)';
+        newBox.appendChild(newIcon);
+      }
     }
-
   }
 
-  deleteBox(id: any){
+  deleteBox(id: any) {
+    if (id == 3) {
+      this.isArrowDraw = false;
+    }
     this.numBoxes -= 1;
     this.boxes?.box.splice(id, 1);
     this.deletedNum.push(parseInt(id));
   }
 
-  deleteAll(){
-    console.log(this.boxes)
-    for (let i = 0; i < this.numBoxes; i++){
+  deleteAll() {
+    console.log(this.boxes);
+    for (let i = 0; i < this.numBoxes; i++) {
       const elementToRemove = document.getElementById(`${i}`);
       elementToRemove?.remove();
     }
+    this.isArrowDraw = false;
     this.numBoxes = 0;
     this.boxes.deleteBoxes();
-    console.log(this.boxes)
+    console.log(this.boxes);
   }
-
 }
